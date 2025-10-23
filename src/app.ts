b0a0609c6c7b0ae6701a620e70d5b66a7ebfba12
Sabbir Hossain <sabbir2always@gmail.com>
2025-10-23 14:28:23 +0600

    add global error handler

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+import express, { NextFunction, Response, Request } from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+// handling upcaughtException
+process.on("uncaughtException", (err) => {
+  console.log(err.message, err.message);
+  process.exit(1);
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", () => {
+  console.log("Server is running");
+});
+
+// sending response for undefined route
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "not-found",
+    message: `Cannot find ${req.originalUrl} on the server`,
+  });
+});
+
+// global error handling middleware
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(err);
+  res.status(err.statusCode || 403).json({
+    status: "error",
+    message: err,
+  });
+});
+
+const serverApp = http.createServer(app);
+// const io = new Server(serverApp, {});
+
+// io.on("connection", (socket) => {
+//   // ...
+//   console.log("Connection established");
+//   // console.log(socket);
+//   socket.on("user_message", (message) => {
+//     if (message) {
+//       socket.emit("hello", "world");
+//     }
+//   });
+// });
+
+export default serverApp;
