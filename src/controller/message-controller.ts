321fb237851f4f213d315974b42b1adcafe45c41
Sabbir Hossain <sabbir2always@gmail.com>
2025-11-05 14:33:10 +0600

    add method to create conversation for better inbox sorting

--- src/controller/message-controller.ts
+++ src/controller/message-controller.ts
@@ -1,21 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import Message from "../model/Message";
 import catchAsync from "../utils/catch-async";
-import Group from "../model/Group";
+import Conversation from "../model/Conversation";
+import { CustomRequest } from "../types";
 
 export const getInbox = catchAsync(async function (
   req: Request,
   res: Response,
   next: NextFunction
 ) {
+  const selfId = (req as CustomRequest).user.id;
   // inbox should have the recent message with friend
+  const conversation = await Conversation.find({
+    participant: { $in: selfId },
+  });
 
-  const groupes = res.status(200).json({
+  res.status(200).json({
     status: "success",
     message: "Inbox retrieved sucessfully",
     data: {
-      friends: [],
-      groups: [],
+      conversation,
     },
   });
 });
--- src/socket/socket-manager.ts
+++ src/socket/socket-manager.ts
@@ -1,5 +1,8 @@
 import { Socket, Server } from "socket.io";
-import { Message as MessageType } from "../types";
+import {
+  Conversation as ConversationType,
+  Message as MessageType,
+} from "../types";
 import { verifyJwtSignature } from "../helpers/jwt-helper";
 import Message from "../model/Message";
 import Conversation from "../model/Conversation";
@@ -42,6 +45,9 @@ export class SocketManager {
 
     // sendMessage
     this.socket.on("send_message", this.sendMessage.bind(this));
+    this.socket.on("send_message_group", (message) => {
+      this.io.to(room as string).emit("user_message", message);
+    });
     this.socket.emit("user_online", `${JSON.stringify(userOnline)}`);
   }
 
@@ -68,32 +74,37 @@ export class SocketManager {
     // get the message send it to the
   }
 
+  async getConversation(receiverId: ObjectId, lastMessage: string) {
+    let conversation: ConversationType | null;
+    conversation = await Conversation.findOne({
+      participant: { $all: [this.userId, receiverId] },
+    });
+
+    // create conversation if doesn't exist
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participant: [this.userId, receiverId],
+        lastMessage: lastMessage,
+      });
+    }
+    return conversation;
+  }
+
   async sendMessage(messageBody: MessageType) {
     if (!messageBody.receiver || !messageBody.message)
       throw new Error("Message needs a receiver and message content");
 
-    // create conversation if doesn't exist
-    let conversationId: ObjectId;
-
-    const conversationExist = await Conversation.findOne({
-      participant: { $all: [this.userId, messageBody.receiver] },
-    });
-    if (conversationExist?.id) {
-      conversationId = conversationExist.id;
-    } else {
-      const newConversation = await Conversation.create({
-        participant: [this.userId, messageBody.receiver],
-        lastMessage: messageBody.message,
-      });
-      conversationId = newConversation.id;
-    }
+    const conversation = await this.getConversation(
+      messageBody.receiver as ObjectId,
+      messageBody.message
+    );
 
     // find the socket id from receiverId from the userOnline list.
     const message: Partial<MessageType> = {
       sender: this.userId,
       receiver: messageBody.receiver,
       message: messageBody.message,
-      conversation: conversationId,
+      conversation: conversation.id,
     };
 
     // if user online then send the message to the socket client
@@ -101,13 +112,14 @@ export class SocketManager {
     try {
       const savedMessage = await Message.create(message);
       // update he converstation last message and timestamps
-      if (conversationExist) {
-        await conversationExist?.updateOne({
+      if (conversation) {
+        await conversation.updateOne({
           lastMessage: savedMessage.message,
           lastMessageTime: savedMessage.createdAt,
         });
       }
 
+      // sending message through socket
       if (userSocket) this.io.to(userSocket).emit("user_message", savedMessage);
     } catch (error: any) {
       if (error) {
